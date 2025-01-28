2d9a62a94190b00b06958090f055ca492e17c931
narayanan13 <narayananofficial13@gmail.com>
2025-01-28 22:03:58 +0530

    login form initialcode

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,8 +1,11 @@
 export default () => {
-    return (
-        <>
-            <h1>Login</h1>
-            <p>This is the Login page</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Login</h1>
+      <form>
+        <input type="email" name="email" placeholder="e.g. abc@gmail.com" />
+        <input type="submit" value="Login" />
+      </form>
+    </>
+  );
 }
